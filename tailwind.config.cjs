a637c7507e3ae185fda5c0a7e0cf0b55870118e4
Lukas Frey <lukas@berenzheinzmann.com>
2022-11-15 23:29:09 +0100

    start implementing design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#12191EDB',
+        accent: '#6388B2',
+        accentHover: '#4773a6',
+        bright: '#F6F6F6',
+      },
+      fontFamily: {
+        sans: ['Rubik'],
+      },
+    },
+  },
+  plugins: [],
+};
